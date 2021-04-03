e3f694b8a9217884d1cca064f6d7747521d895fb
Leandro Real <leo.real2585@gmail.com>
2021-04-03 18:21:48 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/SignUp/SignUpController.ts
+++ src/presentation/controllers/SignUp/SignUpController.ts
@@ -1,18 +1,21 @@
 import { badRequest, serverError, ok } from '../../helpers/HttpHelper'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { AddAccount, EmailValidator, Controller, HttpResponse, HttpRequest } from './SignUpProtocols'
+import { AddAccount, EmailValidator, Controller, HttpResponse, HttpRequest, Validation } from './SignUpProtocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
+  private readonly validation: Validation
 
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
     this.emailValidator = emailValidator
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const requiredFields = ['name', 'email', 'password', 'confirmPassword']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
--- src/presentation/controllers/SignUp/SignUpProtocols.ts
+++ src/presentation/controllers/SignUp/SignUpProtocols.ts
@@ -1,3 +1,4 @@
 export * from '../../protocols'
 export * from '@domain/usecases/AddAccount'
 export * from '@domain/models/Account'
+export * from '../../helpers/validators/validation'
