dba124f069be4d7b631de9883e74ba2f14803c4c
Leandro Real <leo.real2585@gmail.com>
2021-04-14 07:56:56 -0300

    test: login route returns 401 if invalid credentials

--- src/main/routes/Login.routes.test.ts
+++ src/main/routes/Login.routes.test.ts
@@ -47,5 +47,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'johndoe@mail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
