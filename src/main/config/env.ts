6dd8fd6151d1c50335b71a90a6776334648578b1
Leandro Real <leo.real2585@gmail.com>
2021-04-14 07:46:08 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-server',
-  port: process.env.SERVER_PORT || 3333
+  port: process.env.SERVER_PORT || 3333,
+  jwtSecret: process.env.JWT_SECRET || 'default'
 }
