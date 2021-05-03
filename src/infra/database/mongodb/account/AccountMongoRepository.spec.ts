3b70513d7537b4cab03c2a33ed8af44249a2e033
Leandro Real <leo.real2585@gmail.com>
2021-05-03 09:22:21 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/database/mongodb/account/AccountMongoRepository.spec.ts
+++ src/infra/database/mongodb/account/AccountMongoRepository.spec.ts
@@ -57,6 +57,30 @@ describe('Account Mongo Repository', () => {
     })
   })
 
+  describe('loadByToken()', () => {
+    test('should return an account on loadByToken without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    test('should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
+  })
+
   describe('updateAccessToken()', () => {
     test('should update the account accessToken on updateAccessToken success', async () => {
       const sut = makeSut()
