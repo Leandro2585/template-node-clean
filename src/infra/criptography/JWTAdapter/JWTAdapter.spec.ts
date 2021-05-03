a0afea0e258f118fb9528e3b2f9e3b7dd9ba56d5
Leandro Real <leo.real2585@gmail.com>
2021-05-03 08:59:32 -0300

    test: ensure JWTAdapter call verify with correct values

--- src/infra/criptography/JWTAdapter/JWTAdapter.spec.ts
+++ src/infra/criptography/JWTAdapter/JWTAdapter.spec.ts
@@ -4,6 +4,9 @@ import { JWTAdapter } from './JWTAdapter'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return new Promise(resolve => resolve('any_token'))
+  },
+  async verify (): Promise<string> {
+    return new Promise(resolve => resolve('any_value'))
   }
 }))
 
@@ -33,6 +36,15 @@ describe('JWT Adapter', () => {
       })
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    test('should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
   })
 })
