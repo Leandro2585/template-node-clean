28206d2a066fc96059aa66744971f94fc68f6706
Leandro Real <leo.real2585@gmail.com>
2021-05-04 14:23:49 -0300

    refactor: organize SurveyMongoRepository tests in describes

--- src/infra/database/mongodb/survey/SurveyMongoRepository.spec.ts
+++ src/infra/database/mongodb/survey/SurveyMongoRepository.spec.ts
@@ -4,6 +4,10 @@ import { SurveyMongoRepository } from './SurveyMongoRepository'
 
 let surveyCollection: Collection
 
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -15,23 +19,26 @@ describe('Account Mongo Repository', () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
   })
-  const makeSut = (): SurveyMongoRepository => {
-    return new SurveyMongoRepository()
-  }
 
-  test('should create a survey on create success', async () => {
-    const sut = makeSut()
-    await sut.create({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }, {
-        answer: 'other_answer'
-      }],
-      date: new Date()
+  describe('create()', () => {
+    test('should create a survey on create success', async () => {
+      const sut = makeSut()
+      await sut.create({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  })
+
+  describe('loadAll()', () => {
+
   })
 })
