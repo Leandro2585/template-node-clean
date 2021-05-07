6f3a3c5def204c1f4d4f455b844c15afe639faf9
Leandro Real <leo.real2585@gmail.com>
2021-05-07 10:57:51 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/shared/controllers/surveyresult/SaveSurveyResult/SaveSurveyResultController.ts
+++ src/shared/controllers/surveyresult/SaveSurveyResult/SaveSurveyResultController.ts
@@ -1,13 +1,17 @@
 import { InvalidParamError } from '@shared/errors'
 import { forbidden, serverError } from '@shared/helpers/http/HttpHelper'
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './SaveSurveyResultControllerProtocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, SaveSurveyResult } from './SaveSurveyResultControllerProtocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
+      const { accountId } = httpRequest
       const { answer } = httpRequest.body
       const survey = await this.loadSurveyById.loadById(surveyId)
       if (survey) {
@@ -18,6 +22,12 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.saveSurveyResult.save({
+        accountId,
+        surveyId,
+        answer,
+        date: new Date()
+      })
       return null
     } catch (error) {
       return serverError(error)
--- src/shared/controllers/surveyresult/SaveSurveyResult/SaveSurveyResultControllerProtocols.ts
+++ src/shared/controllers/surveyresult/SaveSurveyResult/SaveSurveyResultControllerProtocols.ts
@@ -1,3 +1,5 @@
 export * from '@shared/protocols'
 export * from '@domain/usecases/LoadSurveyById'
 export * from '@domain/models/Survey'
+export * from '@domain/usecases/SaveSurveyResult'
+export * from '@domain/models/SurveyResult'
