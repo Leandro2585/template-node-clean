2515ec4b5fb3f6eb8f354677ec2f21060b80fe60
Leandro Real <leo.real2585@gmail.com>
2021-05-09 19:24:53 -0300

    feat: add no cache middleware

---
+++ src/main/middlewares/NoCache.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express'
+
+export const noCache = (request: Request, response: Response, next: NextFunction): void => {
+  response.set('cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  response.set('pragma', 'no-cache')
+  response.set('expires', '0')
+  response.set('surrogate-control', 'no-store')
+  next()
+}
