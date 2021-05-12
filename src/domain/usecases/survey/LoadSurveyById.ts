b2010f8581c0f7155a1e7b37360ed7f63e4b1372
Leandro Real <leo.real2585@gmail.com>
2021-05-12 15:12:06 -0300

    feat: add LoadSurveyResult use case

--- src/domain/usecases/survey/LoadSurveyById.ts
+++ src/domain/usecases/survey/LoadSurveyById.ts
@@ -1,5 +1,5 @@
 import { SurveyModel } from '@domain/models/Survey'
 
 export interface LoadSurveyById {
-  loadById(id: string): Promise<SurveyModel>
+  loadById(surveyId: string): Promise<SurveyModel>
 }
