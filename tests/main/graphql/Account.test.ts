157e571a3a33681fb575ea033ff10a58b9e618da
Leandro Real <leo.real2585@gmail.com>
2021-05-21 21:14:58 -0300

    test: ensure SignUp Mutation returns an account on valid data

--- tests/main/graphql/Account.test.ts
+++ tests/main/graphql/Account.test.ts
@@ -59,4 +59,33 @@ describe('Account GraphQL', () => {
       expect(response.errors[0].message).toBe('Unauthorized')
     })
   })
+  describe('SignUp Mutation', () => {
+    const signUpMutation = gql`
+      mutation signup($name: String!, $email: String!, $password: String!, $confirmPassword: String!) {
+        signup(name, $name, email: $email, password: $password, confirmPassword: $confirmPassword) {
+          accessToken,
+          name
+        }
+      }
+    `
+    test('should return an Account on valid data', async () => {
+      const password = await hash('123456', 12)
+      await accountCollection.insertOne({
+        name: 'John Doe',
+        email: 'johndoe@mail.com',
+        password
+      })
+      const { query } = createTestClient({ apolloServer })
+      const response: any = await query(signUpMutation, {
+        variables: {
+          name: 'John Doe',
+          email: 'johndoe@mail.com',
+          password: '123456',
+          confirmPassword: '123456'
+        }
+      })
+      expect(response.data.signup.accessToken).toBeTruthy()
+      expect(response.data.signup.name).toBe('John Doe')
+    })
+  })
 })
